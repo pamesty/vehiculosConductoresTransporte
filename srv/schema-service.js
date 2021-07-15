ea6e247e8e8367000090404c7def3f8239751460
pamesty <p.amesty.guanipa@accenture.com>
2021-07-15 14:43:47 -0300

    Optimizacióon de funcion para cargar los filtros

--- srv/schema-service.js
+++ srv/schema-service.js
@@ -1,89 +1,118 @@
 const cds = require('@sap/cds');
 const { OIGC } = cds.entities;
 
+// Función de Filtros
+const fFilter = (selectLength, vObject, vValueKey,vText) => {
+    if (selectLength > 0) {
+        vObject = {
+            valueKey: vValueKey,
+            text: vText,
+            data: selectLength
+        }
+    } else {
+        vObject = {
+            valueKey: vValueKey,
+            text: vText,
+            data: 0
+        }
+    }
+    
+    return vObject;
+}
+
 module.exports = cds.service.impl(async (srv) => {
 
+    // Filtros UT
     srv.on('filtersUT', async () => {
         const tx = srv.tx();
 
-        // Función para cargar la lista de filtros
-        const fQUery = async (vField, vValueKey, vObject, vText) => {
-            let aSelect = '';
+        // Función para sacar el aSelect.length (la cantidad de datos existentes por filtro)
+        const aQuery = async (vField, vValueKey) => {
+            let selectLenght = 0;
 
-            if (vField === 'TU_STATUS') {
-                aSelect = await tx.run(SELECT('TU_STATUS').from(OIGC).where({ TU_STATUS: vValueKey }));
-            } else if (vField === 'TU_MODE') {
-                aSelect = await tx.run(SELECT('TU_MODE').from(OIGC).where({ TU_MODE: vValueKey }));
-            } else if (vField === 'TU_TYPE') {
-                aSelect = await tx.run(SELECT('TU_TYPE').from(OIGC).where({ TU_TYPE: vValueKey }));
-            } else if (vField === 'TU_CLASS') {
-                aSelect = await tx.run(SELECT('TU_CLASS').from(OIGC).where({ TU_CLASS: vValueKey }));
-            }
-
+            let aSelect = await tx.run(SELECT(vField).from(OIGC).where(`${vField} = '${vValueKey}'`));
             if (aSelect.length > 0 && aSelect !== null && aSelect) {
-                vObject = {
-                    valueKey: vValueKey,
-                    text: vText,
-                    data: aSelect.length
-                }
+                selectLenght = aSelect.length;
             } else {
-                vObject = {
-                    valueKey: vValueKey,
-                    text: vText,
-                    data: 0
-                }
+                selectLenght = 0;
             }
-            return vObject;
+
+            return selectLenght;
         }
 
+        // Declaración de variables utilizadas en las funciones
+        let statusLength0 = 0, statusLength1 = 0, statusLength2 = 0;
         let oFilterStatus0 = {}, oFilterStatus1 = {}, oFilterStatus2 = {};
+        let modoUTLength1 = 0, modoUTLength2 = 0; modoUTLength3 = 0, modoUTLength4 = 0, modoUTLength5 = 0;
         let oFilterModoUT1 = {}, oFilterModoUT2 = {}, oFilterModoUT3 = {}, oFilterModoUT4 = {}, oFilterModoUT5 = {};
+        let tipoLengthPHSX = 0, tipoLengthZREF = 0; tipoLengthZTRT = 0, tipoLengthZTET = 0, tipoLengthZTEF = 0;
         let oTipoPHSX = {}, oTipoZREF = {}, oTipoZTRT = {}, oTipoZTET = {}, oTipoZTEF = {};
+        let modoUTTLength1 = 0, modoUTTLength2 = 0, modoUTTLength3 = 0, modoUTTLength4 = 0;
         let oModoUUTT1 = {}, oModoUUTT2 = {}, oModoUUTT3 = {}, oModoUUTT4 = {};
 
         // TU_STATUS
-        oFilterStatus0 = await fQUery('TU_STATUS', '0', oFilterStatus0, 'Disponible');
-        oFilterStatus1 = await fQUery('TU_STATUS', '1', oFilterStatus1, 'No Disponible');
-        oFilterStatus2 = await fQUery('TU_STATUS', '2', oFilterStatus2, 'A Borrar');
+        statusLength0 = await aQuery('TU_STATUS', '0');
+        oFilterStatus0 = await fFilter(statusLength0,oFilterStatus0,'0','Disponible');
+        statusLength1 = await aQuery('TU_STATUS', '1');
+        oFilterStatus1 = await fFilter(statusLength1,oFilterStatus1,'1','No Disponible');
+        statusLength2 = await aQuery('TU_STATUS', '2');
+        oFilterStatus2 = await fFilter(statusLength2,oFilterStatus2,'2','A Borrar');
 
         let aFilterStatus = {
             filterType: 'Status',
             filterKey: 'TU_STATUS',
             value: [oFilterStatus0, oFilterStatus1, oFilterStatus2]
         };
+        console.log(aFilterStatus);
 
         // TU_MODE
-        oFilterModoUT1 = await fQUery('TU_MODE', '1', oFilterModoUT1, 'Carretera');
-        oFilterModoUT2 = await fQUery('TU_MODE', '2', oFilterModoUT2, 'Ferrocarril');
-        oFilterModoUT3 = await fQUery('TU_MODE', '3', oFilterModoUT3, 'Mar');
-        oFilterModoUT4 = await fQUery('TU_MODE', '4', oFilterModoUT4, 'Gabarra');
-        oFilterModoUT5 = await fQUery('TU_MODE', '5', oFilterModoUT5, 'Pipeline');
+        modoUTLength1 = await aQuery('TU_MODE', '1');
+        oFilterModoUT1 = await fFilter(modoUTLength1,oFilterModoUT1,'1','Carretera');
+        modoUTLength2 = await aQuery('TU_MODE', '2');
+        oFilterModoUT2 = await fFilter(modoUTLength2,oFilterModoUT2,'2','Ferrocarril');
+        modoUTLength3 = await aQuery('TU_MODE', '3');
+        oFilterModoUT3 = await fFilter(modoUTLength3,oFilterModoUT3,'3','Mar');
+        modoUTLength4 = await aQuery('TU_MODE', '4');
+        oFilterModoUT4 = await fFilter(modoUTLength4,oFilterModoUT4,'4','Gabarra');
+        modoUTLength5 = await aQuery('TU_MODE', '5');
+        oFilterModoUT5 = await fFilter(modoUTLength5,oFilterModoUT5,'5','Pipeline');
 
         let aFilterModeUT = {
             filterType: 'ModoUT',
             filterKey: 'TU_MODE',
             value: [oFilterModoUT1, oFilterModoUT2, oFilterModoUT3, oFilterModoUT4, oFilterModoUT5]
         }
+        console.log(aFilterModeUT);
 
         // TU_TYPE
-        oTipoPHSX = await fQUery('TU_TYPE', 'PHSX', oTipoPHSX, 'PHSX');
-        oTipoZREF = await fQUery('TU_TYPE', 'ZREF', oTipoZREF, 'ZREF');
-        oTipoZTRT = await fQUery('TU_TYPE', 'ZTRT', oTipoZTRT, 'ZTRT');
-        oTipoZTET = await fQUery('TU_TYPE', 'ZTET', oTipoZTET, 'ZTET');
-        oTipoZTEF = await fQUery('TU_TYPE', 'ZTEF', oTipoZTEF, 'ZTEF');
+        tipoLengthPHSX = await aQuery('TU_TYPE', 'PHSX');
+        oTipoPHSX = await fFilter(tipoLengthPHSX,oTipoPHSX,'PHSX','PHSX');
+        tipoLengthZREF = await aQuery('TU_TYPE', 'ZREF');
+        oTipoZREF = await fFilter(tipoLengthZREF,oTipoZREF,'ZREF','ZREF');
+        tipoLengthZTRT = await aQuery('TU_TYPE', 'ZTRT');
+        oTipoZTRT = await fFilter(tipoLengthZTRT,oTipoZTRT,'ZTRT','ZTRT');
+        tipoLengthZTET = await aQuery('TU_TYPE', 'ZTET');
+        oTipoZTET = await fFilter(tipoLengthZTET,oTipoZTET,'ZTET','ZTET');
+        tipoLengthZTEF = await aQuery('TU_TYPE', 'ZTEF');
+        oTipoZTEF = await fFilter(tipoLengthZTEF,oTipoZTET,'ZTEF','ZTEF');
 
         let aFilterTipoUT = {
             filterType: 'TipoUT',
             filterKey: 'TU_TYPE',
             value: [oTipoPHSX, oTipoZREF, oTipoZTRT, oTipoZTET, oTipoZTEF]
         }
+        console.log(aFilterTipoUT);
 
         // TU_CLASS
-        oModoUUTT1 = await fQUery('TU_CLASS', '1', oModoUUTT1, 'Máquina motriz (sin capacidad de carga)');
-        oModoUUTT2 = await fQUery('TU_CLASS', '2', oModoUUTT2, 'Remolque (sin motor)');
-        oModoUUTT3 = await fQUery('TU_CLASS', '3', oModoUUTT3, 'Unidad individual con motor y capacidad de carga');
-        oModoUUTT4 = await fQUery('TU_CLASS', '4', oModoUUTT4, 'Otras unidades que sostienen peso (sin capacidad de carga)');
-
+        modoUTTLength1 = await aQuery('TU_CLASS', '1');
+        oModoUUTT1 = await fFilter(modoUTTLength1,oModoUUTT1,'1','Máquina motriz (sin capacidad de carga)');
+        modoUTTLength2 = await aQuery('TU_CLASS', '2');
+        oModoUUTT2 = await fFilter(modoUTTLength2,oModoUUTT2,'2','Remolque (sin motor)');
+        modoUTTLength3 = await aQuery('TU_CLASS', '3');
+        oModoUUTT3 = await fFilter(modoUTTLength3,oModoUUTT3,'3','Unidad individual con motor y capacidad de carga');
+        modoUTTLength4 = await aQuery('TU_CLASS', '4');
+        oModoUUTT4 = await fFilter(modoUTTLength4,oModoUUTT4,'4','Otras unidades que sostienen peso (sin capacidad de carga)');
+        
         let aFilterModoUUTT = {
             filterType: 'Modo de UUTT',
             filterKey: 'TU_CLASS',
@@ -94,42 +123,7 @@ module.exports = cds.service.impl(async (srv) => {
         console.log(aFilter);
 
         return aFilter;
-    });
 
-    srv.on('filtersUT', async () => {
-        const tx = srv.tx();
-
-        // Función para cargar la lista de filtros
-        const fQUery = async (vField, vValueKey, vObject, vText) => {
-            let aSelect = '';
-
-            if (vField === 'TU_STATUS') {
-                aSelect = await tx.run(SELECT('TU_STATUS').from(OIGC).where({ TU_STATUS: vValueKey }));
-            } else if (vField === 'TU_MODE') {
-                aSelect = await tx.run(SELECT('TU_MODE').from(OIGC).where({ TU_MODE: vValueKey }));
-            } else if (vField === 'TU_TYPE') {
-                aSelect = await tx.run(SELECT('TU_TYPE').from(OIGC).where({ TU_TYPE: vValueKey }));
-            } else if (vField === 'TU_CLASS') {
-                aSelect = await tx.run(SELECT('TU_CLASS').from(OIGC).where({ TU_CLASS: vValueKey }));
-            }
-
-            if (aSelect.length > 0 && aSelect !== null && aSelect) {
-                vObject = {
-                    valueKey: vValueKey,
-                    text: vText,
-                    data: aSelect.length
-                }
-            } else {
-                vObject = {
-                    valueKey: vValueKey,
-                    text: vText,
-                    data: 0
-                }
-            }
-            return vObject;
-        }
-
-        
     });
 
 });
